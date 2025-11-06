9cf693e76f6b0a92d9d4f6aecf3d24b759d3339f
Jyotshna2007 <kondepudijyotshna@gmail.com>
2025-11-06 21:21:56 +0530

    Time: 839 ms (15.5%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+select d.name as department,e.name as employee,e.salary as salary
+from employee e
+left join department d
+on e.departmentid=d.id
+where(e.departmentid,e.salary) in (
+ select departmentid,max(salary) from employee
+ group by departmentid
+);
