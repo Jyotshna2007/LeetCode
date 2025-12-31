ec72edbef47e61d3bebaa66b41429263d8afd4d6
Jyotshna2007 <kondepudijyotshna@gmail.com>
2025-12-31 19:57:13 +0530

    Time: 0 ms (100%), Space: 13.7 MB (5.24%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int res;
+        for(int i=0;i<nums.size();i++){
+            if(target<=nums[i]){
+                res=i;
+                break;
+            }
+        }
+        return res;
+    }
+};
