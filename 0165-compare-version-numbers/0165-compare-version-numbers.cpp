4e9e9d2f67391c64d8dd752143ac7c5043a4d7b2
Jyotshna2007 <kondepudijyotshna@gmail.com>
2026-02-23 20:56:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.61%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        string s;
+        vector<int> v1, v2;
+        for (char c : version1) {
+            if (c == '.') {
+                v1.push_back(stoi(s));
+                s.clear();
+            } else
+                s += c;
+        }
+        if (!s.empty()) {
+            v1.push_back(stoi(s));
+            s.clear();
+        }
+        for (char c : version2) {
+            if (c == '.') {
+                v2.push_back(stoi(s));
+                s.clear();
+            } else
+                s += c;
+        }
+        if (!s.empty()) {
+            v2.push_back(stoi(s));
+            s.clear();
+        }
+        int n = max(v1.size(), v2.size());
+        for (int i = 0; i < n; i++) {
+            int a = (i < v1.size() ? v1[i] : 0);
+            int b = (i < v2.size() ? v2[i] : 0);
+            if (a < b)
+                return -1;
+            if (a > b)
+                return 1;
+        }
+        return 0;
+    }
+};
