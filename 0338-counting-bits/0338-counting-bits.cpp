cbb736b5a520c1854edbc51c856f16deb2a8f525
Jyotshna Kondepudi <kondepudijyotshna@gmail.com>
2026-08-10 14:55:16 +0530

    Time: 3 ms (23.43%), Space: 11.2 MB (29.19%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>res;
-
-        for(int i=0;i<=n;i++){
-            int s=i,cnt=0;
-            while(s){
-   int r=s%2;
-   if(r==1) cnt++;
-   s=s/2;
+        vector<int> res;
+        for (int i = 0; i <= n; i++) {
+            int s = i, cnt = 0;
+            while (s) {
+                int r = s % 2;
+                if (r == 1)
+                    cnt++;
+                s = s / 2;
             }
             res.push_back(cnt);
         }
