7165a9871df894ad3734713b5f2d2a050816279e
Jyotshna Kondepudi <kondepudijyotshna@gmail.com>
2026-08-11 06:18:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.79%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        string s;
-        int x = 32;
-        while (x) {
-            int rem = n % 2;
-            s.push_back(rem + '0');
-            n = n / 2;
-            x--;
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>= 1;
         }
-        long long ans = 0;
-        int j = 0;
-        for (int i = s.size() - 1; i >= 0; i--) {
-            if (s[i] == '1')
-                ans += pow(2, j);
-            j++;
-        }
-        return ans;
+        return res;
     }
 };
