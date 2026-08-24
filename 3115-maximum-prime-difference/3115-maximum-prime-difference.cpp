d06e32fde58ce25abf58e53a47f6907de2da7edf
Jyotshna Kondepudi <kondepudijyotshna@gmail.com>
2026-08-24 16:02:11 +0530

    Time: 100 ms (27.91%), Space: 111.6 MB (17.76%) - LeetHub

---
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        if (n <= 1)
+            return false;
+        if (n <= 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        for (int i = 5; i * i <= n; i += 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        return true;
+    }
+    int maximumPrimeDifference(vector<int>& nums) {
+        vector<int> chum;
+        for (int i = 0; i < nums.size(); i++) {
+            if (isPrime(nums[i])) {
+                chum.push_back(i);
+            }
+        }
+        return chum.back() - chum.front();
+    }
+};
