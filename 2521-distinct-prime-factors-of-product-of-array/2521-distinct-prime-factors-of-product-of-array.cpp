9901db4e131014972583f0e3274c93ec2495e69e
Jyotshna Kondepudi <kondepudijyotshna@gmail.com>
2026-08-24 15:34:45 +0530

    Time: 40 ms (21.1%), Space: 59.2 MB (5.07%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-const int maxi=1e5+1;
-vector<int>spf;
-void arr(){
-    spf.resize(maxi);
-    for(int i=0;i<maxi;i++){
-        spf[i]=i;
-    }
-    for(int i=2;i*i<maxi;i++){
-        if(spf[i]==i){
-            for(int j=i*i;j<maxi;j+=i){
-                if(spf[j]==j) spf[j]=i;
+    const int maxi = 1e5 + 1;
+    vector<int> spf;
+    void arr() {
+        spf.resize(maxi);
+        for (int i = 0; i < maxi; i++) {
+            spf[i] = i;
+        }
+        for (int i = 2; i * i < maxi; i++) {
+            if (spf[i] == i) {
+                for (int j = i * i; j < maxi; j += i) {
+                    if (spf[j] == j)
+                        spf[j] = i;
+                }
             }
         }
     }
-
-}
     int distinctPrimeFactors(vector<int>& nums) {
         arr();
-        unordered_map<int,int>s;
-        for(int i=0;i<nums.size();i++){
-            int j=nums[i];
-            while(j>1){
-            int p=spf[j];
-            s[p]++;
-            j/=p;
+        unordered_map<int, int> s;
+        for (int i = 0; i < nums.size(); i++) {
+            int j = nums[i];
+            while (j > 1) {
+                int p = spf[j];
+                s[p]++;
+                j /= p;
             }
         }
         return s.size();
