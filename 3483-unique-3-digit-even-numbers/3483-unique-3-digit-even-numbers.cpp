c7e8d5e792f1c52fcfca693953a35a06432b63fc
Jyotshna Kondepudi <kondepudijyotshna@gmail.com>
2026-09-20 07:26:55 +0530

    Time: 0 ms (100%), Space: 29.9 MB (52.35%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
+int cnt=0;
+vector<bool>used;
+void fun(int idx,string &ans,vector<int>&digits){
+    if(ans.size()==3){
+        if((ans[2]-'0')%2==0) cnt++;
+        return;
+    }
+    bool seen[10]={false};
+    for(int i=0;i<digits.size();i++){
+        if(used[i]) continue;
+        if(ans.empty() && digits[i]==0) continue;
+        if(seen[digits[i]]) continue;
+        seen[digits[i]]=true;
+        ans+=char(digits[i]+'0');
+        used[i]=true;
+        fun(idx+1,ans,digits);
+        ans.pop_back();
+        used[i]=false;
+    }
+}
     int totalNumbers(vector<int>& digits) {
-        unordered_set<int>chum;
-        int n=digits.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                for(int k=0;k<n;k++){
-                    if(i!=j && i!=k && j!=k){
-                    if(digits[i]!=0){
-                        if(digits[k]%2==0){
-              int num=digits[i]*100+digits[j]*10+digits[k];
-              chum.insert(num);              
-                        }
-                    }
-                }
-                }
-            }
-        }
-        return chum.size();
+        string ans="";
+        used.resize(digits.size(),false);
+         fun(0,ans,digits);
+         return cnt;
     }
 };
